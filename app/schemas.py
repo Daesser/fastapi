033a260801a4a814643b19232cf35f3399c3049d
Daniel Lam <daniel.lam@bluewin.ch>
2023-11-21 22:19:53 +0100

    added test and github action

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 from typing import Optional
-
+from typing_extensions import Annotated
 from pydantic.types import conint
 
 
@@ -63,4 +63,4 @@ class TokenData(BaseModel):
 
 class Vote(BaseModel):
     post_id: int
-    dir: conint(le=1)
+    dir: Annotated[int, Field(strict=True, le=1)]
